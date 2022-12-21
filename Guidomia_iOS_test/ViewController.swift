f1c004304ce52494f6dab38c3a9b9509cc21b54d
mohammed <medhajajate@gmail.com>
2022-12-21 22:05:42 +0100

    Initial Commit

---
+++ Guidomia_iOS_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guidomia_iOS_test
+//
+//  Created by med hajajate on 21/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Guidomia_iOS_testUITests/Guidomia_iOS_testUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Guidomia_iOS_testUITestsLaunchTests.swift
+//  Guidomia_iOS_testUITests
+//
+//  Created by med hajajate on 21/12/2022.
+//
+
+import XCTest
+
+class Guidomia_iOS_testUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
